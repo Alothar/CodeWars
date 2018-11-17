d05c8e1c8c901ed41052eab0681a6d13753ac0ad
Serhii <sergomel52@gmail.com>
2018-11-17 19:10:03 +0100

    Update Scrambles.java

--- Scrambles.java
+++ Scrambles.java
@@ -1,7 +1,21 @@
 public class Scramblies {
     
     public static boolean scramble(String str1, String str2) {
-    
-        // your code
+      int[] arr = new int[26];
+      for (char c : str2.toCharArray())
+      {
+        arr[c - 'a']++;
+      }
+      for (char c : str1.toCharArray())
+      {
+        arr[c - 'a']--;
+      }
+      for (int value : arr) 
+      {
+        if (value > 0) {
+            return false;
+      }
+    }
+    return true;
     }
 }
